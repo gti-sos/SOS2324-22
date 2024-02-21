16d953149151b92614ce4d1e279d781039376da7
Juan Ros <jrosraposo@gmail.com>
2024-02-21 11:15:04 +0100

    fix index.js

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ let express = require("express");
 
 let app = express();
 
+const PORT = (process.env.PORT || 10000);
+
 app.use("/",express.static("./public"));
 
 
 app.get("/cool", (req,res)=>{
     res.send(`<html><body><h1>${cool()}</h1><body><html>`);
+});
+
+app.listen(PORT, ()=> {
+    console.log(`server listening on ${PORT}` );
 });
