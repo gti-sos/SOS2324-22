acf239bf6bf93ac68fa116c056eab20d8296bae8
rubgonama <53659854+rubgonama@users.noreply.github.com>
2024-02-25 22:09:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ app.use("/samples/DTN",express.static("./samples/DTN"));
 
 app.use("/samples/RGA",express.static("./samples/RGA"));
 
+app.use("/samples/BVB",express.static("./samples/BVB"));
+
 app.get("/cool", (req,res)=>{
     res.send(`<html><body><h1>${cool()}</h1><body><html>`);
 });
