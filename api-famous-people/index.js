d8c808fc97125f7fd67e2635adb41b4b0edfb483
juarosrap <jrosraposo@gmail.com>
2024-03-03 11:58:24 +0100

    finish: index

--- api-famous-people/index.js
+++ api-famous-people/index.js
@@ -1,114 +1,176 @@
-const API_BASE = "/api/v1";
+    const API_BASE = "/api/v1";
 
-let initial_list = [
-    { name: 'George Washington',
-      short_description: '1st president of the United States (1732–1799)',
-      gender: 'Male',
-      country: 'United States of America',
-      occupation: 'Politician',
-      birth_year: 1732,
-      death_year: 1799,
-      age_of_death: 67    
-    },
-    { name: 'Douglas Adams',
-      short_description: 'English writer and humorist',
-      gender: 'Male',
-      country: 'United Kingdom',
-      occupation: 'Artist',
-      birth_year: 1952,
-      death_year: 2001,
-      age_of_death: 49    
-    },
-    { name: 'Abraham Lincoln',
-      short_description: '16th president of the United States (1809-1865)',
-      gender: 'Male',
-      country: 'United States of America',
-      occupation: 'Politican',
-      birth_year: 1809,
-      death_year: 1865,
-      age_of_death: 56    
-    },
-    { name: 'Wolfgang Amadeus Mozart',
-      short_description: 'Austrian composer of the Classical period',
-      gender: 'Male',
-      country: 'Archduchy of Austria',
-      occupation: 'Artist',
-      birth_year: 1756,
-      death_year: 1791,
-      age_of_death: 35    
-    },
-    { name: 'Adolf Hitler',
-      short_description: 'chancellor and Führer of Germany, leader of the Nazi Party',
-      gender: 'Male',
-      country: 'Germany',
-      occupation: 'Statesperson',
-      birth_year: 1889,
-      death_year: 1945,
-      age_of_death: 56    
-    },
-    { name: 'J. R. R. Tolkien',
-      short_description: 'British philologist and author of classic high fantasy works',
-      gender: 'Male',
-      country: 'United Kingdom',
-      occupation: 'Researcher',
-      birth_year: 1892,
-      death_year: 1973,
-      age_of_death: 81    
-    },
-    { name: 'Charles Darwin',
-      short_description: 'British naturalist, author of "On the origin of species, by means of natural selection" (1809-1882)',
-      gender: 'Male',
-      country: 'United Kingdom',
-      occupation: 'Researcher',
-      birth_year: 1809,
-      death_year: 1882,
-      age_of_death: 73    
-    },
-    { name: 'John Lennon',
-      short_description: 'English singer and songwriter, founding member of The Beatles (1940-1980)',
-      gender: 'Male',
-      country: 'United Kingdom',
-      occupation: 'Artist',
-      birth_year: 1940,
-      death_year: 1980,
-      age_of_death: 80    
-    },
-    { name: 'Ramesses IV',
-      short_description: 'the third pharaoh of the Twentieth Dynasty of the New Kingdom of Ancient Egypt',
-      gender: 'Male',
-      country: 'Ancient Egypt',
-      occupation: 'Statesperson',
-      birth_year: -1200,
-      death_year: -1149,
-      age_of_death: 51    
-    },
-    { name: 'Louis Armstrong',
-      short_description: 'American jazz trumpeter, composer and singer',
-      gender: 'Male',
-      country: 'United States of America',
-      occupation: 'Artist',
-      birth_year: 1900,
-      death_year: 1971,
-      age_of_death: 71    
-    },
-    { name: 'Michael Jackson',
-      short_description: 'American recording artist; singer and songwriter (1958-2009)',
-      gender: 'Male',
-      country: 'United States of America',
-      occupation: 'Artist',
-      birth_year: 1958,
-      death_year: 2009,
-      age_of_death: 51   
-    }];
+    let initial_list = [
+      { name: 'George Washington',
+        short_description: '1st president of the United States (1732–1799)',
+        gender: 'Male',
+        country: 'United States of America',
+        occupation: 'Politician',
+        birth_year: 1732,
+        death_year: 1799,
+        age_of_death: 67    
+      },
+      { name: 'Douglas Adams',
+        short_description: 'English writer and humorist',
+        gender: 'Male',
+        country: 'United Kingdom',
+        occupation: 'Artist',
+        birth_year: 1952,
+        death_year: 2001,
+        age_of_death: 49    
+      },
+      { name: 'Abraham Lincoln',
+        short_description: '16th president of the United States (1809-1865)',
+        gender: 'Male',
+        country: 'United States of America',
+        occupation: 'Politican',
+        birth_year: 1809,
+        death_year: 1865,
+        age_of_death: 56    
+      },
+      { name: 'Wolfgang Amadeus Mozart',
+        short_description: 'Austrian composer of the Classical period',
+        gender: 'Male',
+        country: 'Archduchy of Austria',
+        occupation: 'Artist',
+        birth_year: 1756,
+        death_year: 1791,
+        age_of_death: 35    
+      },
+      { name: 'Adolf Hitler',
+        short_description: 'chancellor and Führer of Germany, leader of the Nazi Party',
+        gender: 'Male',
+        country: 'Germany',
+        occupation: 'Statesperson',
+        birth_year: 1889,
+        death_year: 1945,
+        age_of_death: 56    
+      },
+      { name: 'J. R. R. Tolkien',
+        short_description: 'British philologist and author of classic high fantasy works',
+        gender: 'Male',
+        country: 'United Kingdom',
+        occupation: 'Researcher',
+        birth_year: 1892,
+        death_year: 1973,
+        age_of_death: 81    
+      },
+      { name: 'Charles Darwin',
+        short_description: 'British naturalist, author of "On the origin of species, by means of natural selection" (1809-1882)',
+        gender: 'Male',
+        country: 'United Kingdom',
+        occupation: 'Researcher',
+        birth_year: 1809,
+        death_year: 1882,
+        age_of_death: 73    
+      },
+      { name: 'John Lennon',
+        short_description: 'English singer and songwriter, founding member of The Beatles (1940-1980)',
+        gender: 'Male',
+        country: 'United Kingdom',
+        occupation: 'Artist',
+        birth_year: 1940,
+        death_year: 1980,
+        age_of_death: 80    
+      },
+      { name: 'Ramesses IV',
+        short_description: 'the third pharaoh of the Twentieth Dynasty of the New Kingdom of Ancient Egypt',
+        gender: 'Male',
+        country: 'Ancient Egypt',
+        occupation: 'Statesperson',
+        birth_year: -1200,
+        death_year: -1149,
+        age_of_death: 51    
+      },
+      { name: 'Louis Armstrong',
+        short_description: 'American jazz trumpeter, composer and singer',
+        gender: 'Male',
+        country: 'United States of America',
+        occupation: 'Artist',
+        birth_year: 1900,
+        death_year: 1971,
+        age_of_death: 71    
+      },
+      { name: 'Michael Jackson',
+        short_description: 'American recording artist; singer and songwriter (1958-2009)',
+        gender: 'Male',
+        country: 'United States of America',
+        occupation: 'Artist',
+        birth_year: 1958,
+        death_year: 2009,
+        age_of_death: 51   
+      }];
+    
+    
+      function validarDatos(req, res, next) {
+        const json = req.body;
+      
+        const esquema = {
+            'name': 'string', 
+            'short_description': 'string', 
+            'gender': 'number', 
+            'country': 'string', 
+            'occupation': 'number', 
+            'birth_year': 'number', 
+            'death_year': 'number', 
+            'age_of_death': 'number'
+        };
+    
+        const receivedKeys = Object.keys(json);
+        const expectedKeys = Object.keys(esquema);
+        const missingKeys = expectedKeys.filter(key => !receivedKeys.includes(key));
+        
+        
+        const extraKeys = receivedKeys.filter(key => !expectedKeys.includes(key));
+        if (extraKeys.length > 0) {
+            console.error(`There are more keys than expected: ${extraKeys.join(', ')}`);
+            return res.sendStatus(400, "Bad request");
+        }
+    
+        
+        if (missingKeys.length > 0) {
+            console.error(`There are missing keys: ${missingKeys.join(', ')}` );
+            return res.sendStatus(400, "Bad request");
+        }
+    
+        
+        const erroresTipo = [];
+
+        expectedKeys.forEach(key => {
+            const tipoEsperado = esquema[key];
+            const valor = json[key];
+            if (typeof valor !== tipoEsperado) {
+                erroresTipo.push(`El valor de '${key}' debe ser de tipo '${tipoEsperado}'`);
+            }
+        });
+    
+        if (erroresTipo.length > 0) {
+            console.error(`Errores de tipo: ${erroresTipo.join(', ')}`);
+            return res.sendStatus(400, "Bad request");
+        }
+        next();
+    }
+
+
 
     
     
     module.exports = (app,dbFamousPeople) => {
       
         app.get(API_BASE+"/loadInitialData", (req, res) => {
-          dbFamousPeople.insert(initial_list);
-          res.sendStatus(200,"Ok");
+          dbFamousPeople.find({}, (err, docs) => {
+            if(err){
+                res.sendStatus(500, "Internal Error");
+            }else {
+                if (docs.length === 0) {
+                    dbFamousPeople.insert(initial_list);
+                    res.sendStatus(201, "Created");
+                } else{
+                    res.sendStatus(409, "Conflict");
+                }
+            }
         });
+    });
 
 
 
@@ -131,14 +193,14 @@ let initial_list = [
 
           dbFamousPeople.findOne( { name: personName }, (err,searchedPerson) => {
             if(err){
-              res.sendStatus(500,"Internal Error");
+              res.sendStatus(404,"Person not found");
             } else{
               res.send(JSON.stringify(searchedPerson));
             }
           })
         });
     
-        app.post(API_BASE+"/famous-people", (req,res)=>{
+        app.post(API_BASE+"/famous-people", validarDatos, (req,res)=>{
             let person = req.body;
 
             dbFamousPeople.findOne({ name: person.name}, (err,existingPerson) => {
@@ -159,6 +221,10 @@ let initial_list = [
               }                 
             });
           });
+
+          app.post(API_BASE+"/famous-people/:name", (req,res) => {
+            res.sendStatus(405, "Method not allowed");
+          })
      
         
         app.delete(API_BASE + "/famous-people/:name", (req, res) => {
@@ -179,8 +245,35 @@ let initial_list = [
           
         
         app.put(API_BASE+"/famous-people", (req,res) =>{
-          let updatedPerson = req.body;
-
+          res.sendStatus(405,"Method not allowed");
         });
-  
+
+
+
+        app.put(API_BASE+"/famous-people/:name", (req,res) => {
+          let personToUpdate = req.params.name;
+          let newData = req.body;
+
+          dbFamousPeople.update({ "name": personToUpdate }, { $set : newData}, (err,numUpdated) => {
+            if (err) {
+              res.sendStatus(400, "Bad request");
+          } else {
+              if (numUpdated === 0) {
+                  res.sendStatus(404, "Not found");
+              } else {
+                  res.sendStatus(200, "Ok");
+              }
+            }
+          });
+        });
+        /*{       
+    "name": "Alberto",
+        "short_description": "1st president of the United States (1732–1799)",
+        "gender": "Male",
+        "country": "United States of America",
+        "occupation": "Politician",
+        "birth_year": 1732,
+        "death_year": 1799,
+        "age_of_death": 40  
+}*/ 
     }
