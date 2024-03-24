3da1d129fbdcbb9a9ff3f336dafacb600008236e
juarosrap <jrosraposo@gmail.com>
2024-03-24 19:49:31 +0100

    add: initial front

---
+++ front/src/routes/famous-people/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import {onMount} from "svelte";
+    let API = "http://localhost:10000/api/v1/famous-people"
+
+    onMount(() => {
+        getPeople();
+    })
+
+    async function getPeople() {
+        
+            let response = await fetch(API,{
+                method: "GET"
+            });
+            let data = await response.json();
+            console.log(data);
+        } 
+            
+    
+        
+</script>
+
