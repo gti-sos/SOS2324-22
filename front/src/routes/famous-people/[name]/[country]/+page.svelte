e73d98ddf44ff3a453bd3662de859a8be010b6e9
juarosrap <jrosraposo@gmail.com>
2024-03-26 13:25:54 +0100

    add: some features

--- front/src/routes/famous-people/[name]/[country]/+page.svelte
+++ front/src/routes/famous-people/[name]/[country]/+page.svelte
@@ -12,22 +12,45 @@
         getPeople();
     })
 
-    async function getPeople(){
+    async function getPeople() {
+
+        const response = await fetch(API, {
+            method: "GET",
+        });
+        try {
+            const data = await res.json();
+            result = JSON.stringify(data, null, 2);
+            updated_name = data.name;
+            updated_country = data.country;
+        } catch (e) {
+            console.log(`Error parsing result`);
+        }
         
+        if(response.status == 404){
+            Msg = `No existe la persona`;
+            setTimeout(() => {
+                Msg = "";
+            }, 3000);
+        }else{
+            if(response.status == 400){
+                Msg = "Ha habido un error en la petición";
+                setTimeout(() => {
+                    Msg = "";
+                }, 3000);
+            }
+        }
     }
     
     async function updatePerson(){
-        const updatedFields = {
-                name: person.name,
-                country: person.country,
-            };
+        const personDetails = await fetch(`http://localhost:10000/famous-people/${person.name}`);
+        const updatedPerson = await personDetails.json(); 
         try{
             let response = await fetch(API,{
                 method: "PUT",
                 headers: {
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify(updatedFields)
+                body: JSON.stringify(updatedPerson)
             });
         if (response.status == 200){
             Msg = "Persona actualizada con éxito"
