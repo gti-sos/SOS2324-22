eb9e3f8a684d490fe67d1abe2ed6b2c7a73e60f8
benvelben <160509273+benvelben@users.noreply.github.com>
2024-05-02 13:48:20 +0200

    Update +page.svelte

--- front/src/routes/top-richest/[name]/[nationality]/+page.svelte
+++ front/src/routes/top-richest/[name]/[nationality]/+page.svelte
@@ -7,7 +7,7 @@
     
 
     let person = $page.params;
-    const API_BASE = 'https://sos2324-22.appspot.com' || 'http://localhost:10000'
+    const API_BASE ='https://sos2324-22.appspot.com' ||'http://localhost:10000'
     const API = `${API_BASE}/api/v2/top-richest/${person.name}/${person.nationality}`;
     let errorMsg = "";
     let Msg = "";
@@ -20,13 +20,6 @@
         const data = await response.json();
         
         try {
-            
-            name = data.name;
-            let net_worth = data.net_worth;
-            let bday_year = data.bday_year;
-            let age = data.age;
-            let nationality = data.nationality;
-    
 
             if(response.status == 404){
             errorMsg = `No existe el millonario`;
@@ -50,9 +43,9 @@
     async function updateMillonario(){
         const updatedPerson = {
                 name: millonarioData?.name,
-                net_worth: millonarioData?.net_worth,
-                bday_year: millonarioData?.bday_year,
-                age: millonarioData?.age,
+                net_worth: parseInt(millonarioData?.net_worth),
+                bday_year: parseInt(millonarioData?.bday_year),
+                age: parseInt(millonarioData?.age),
                 nationality: millonarioData?.nationality,
         };
 
@@ -83,6 +76,74 @@
     
 
 </script>
+
+<style>
+    input {
+        width: 200px; 
+        border: 1px solid #ccc; 
+        padding: 5px; 
+        font-size: 16px; 
+        
+    }
+
+</style>
+
+
+
+<h2>Detalles de {person.name}</h2>
+<hr>
+
+{#if millonarioData}
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Patrimonio Neto</th>
+                        <th>Año de nacimiento</th>
+                        <th>Edad</th>
+                        <th>Nacionalidad</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><input class="form-control" bind:value={millonarioData.name}></td>
+                        <td><input class="form-control" bind:value={millonarioData.net_worth}></td>
+                        <td><input class="form-control" bind:value={millonarioData.bday_year}></td>
+                        <td><input class="form-control" bind:value={millonarioData.age} type="number"></td>
+                        <td><input class="form-control" bind:value={millonarioData.nationality}></td>
+                        
+                    </tr>     
+                        
+                </tbody>
+                 
+            </table>
+            <MessageContainer {Msg} {errorMsg}/>  
+            
+        </div>
+        
+    </div>
+    <div class="col">
+        <div class="col-sm-6">
+            
+            <div class="d-flex justify-content-end mb-3">
+                
+                <Button color="primary" on:click="{updateMillonario}">Actualizar</Button>
+                
+            </div>
+           
+        </div>
+        
+    </div>
+</div>
+
+{/if}
+
+
+
+
 
 <style>
     input {
