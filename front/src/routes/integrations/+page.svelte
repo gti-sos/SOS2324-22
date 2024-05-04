c9d7e6873d135c538670062ee3319bc2f29c7119
juarosrap <jrosraposo@gmail.com>
2024-05-04 16:56:40 +0200

    add: integrations

---
+++ front/src/routes/integrations/+page.svelte
@@ -0,0 +1,65 @@
+<script>
+      import { Button } from '@sveltestrap/sveltestrap';
+</script>
+
+<style>
+    main {
+        display: flex;
+        justify-content: center;
+        align-items: flex-start;
+        height: 100vh;
+    }
+
+    .container {
+        width: 300px;
+        padding: 20px;
+        background-color: #f9f9f9;
+        border-radius: 10px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+
+    section {
+        text-align: center;
+        margin-bottom: 20px;
+    }
+
+    h3 {
+        font-family: Arial, sans-serif;
+        font-size: 20px;
+        margin-bottom: 10px;
+    }
+
+    button {
+        background-color: #007bff;
+        color: #fff;
+        border: none;
+        padding: 10px 20px;
+        font-size: 14px;
+        border-radius: 5px;
+        text-decoration: none;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+    }
+
+    button:hover {
+        background-color: #0056b3;
+    }
+</style>
+
+<main>
+    <div class="container">
+        <section>
+            <h3>Juan</h3>
+            <a href="/dogs" style="text-decoration: none;">
+                <button>Uso API de perros</button>
+            </a>
+        </section>
+
+        <section>
+            <h3>Dani</h3>
+            <a href="/dogs" style="text-decoration: none;">
+                <button>Uso API de fomrula 1</button>
+            </a>
+        </section>
+    </div>
+</main>
