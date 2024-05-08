499172127ae302094d7b56e342338e6eb9b7f1ca
benvelben <160509273+benvelben@users.noreply.github.com>
2024-05-08 12:49:56 +0200

    Update +page.svelte

--- front/src/routes/Integraciones-benvelben/crypto/+page.svelte
+++ front/src/routes/Integraciones-benvelben/crypto/+page.svelte
@@ -1,4 +1,10 @@
 <svelte:head>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/highcharts-more.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"></script>
+    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+    <script src="https://code.highcharts.com/modules/treemap.js"></script>
     <script src="https://code.highcharts.com/modules/bar.js"></script>
 </svelte:head>
 
