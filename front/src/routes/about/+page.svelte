5baff763c5ffc76c9c44848b91f3b16d63b8ebce
juarosrap <jrosraposo@gmail.com>
2024-05-13 18:02:54 +0200

    add: about

--- front/src/routes/about/+page.svelte
+++ front/src/routes/about/+page.svelte
@@ -7,5 +7,58 @@
     }
 </script>
 
+<style>
+     
+    main {
+        display: flex;
+        justify-content: center;
+        align-items: flex-start;
+        height: 100vh;
+    }
 
-<h1>Ahora mismo estás en {mode}</h1>
+    .container {
+        width: 300px;
+        padding: 20px;
+        background-color: #f9f9f9;
+        border-radius: 10px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+
+    section {
+        text-align: center;
+        margin-bottom: 20px;
+    }
+
+    h3 {
+        font-family: Arial, sans-serif;
+        font-size: 20px;
+        margin-bottom: 10px;
+    }
+</style>
+
+<h2>Ahora mismo estás en {mode}</h2>
+<main>
+    
+    <div class="container">
+        <section>
+            <h3>Juan</h3>
+            <a href="https://youtu.be/rgB9fPPpKtM">Vídeo entregable D03</a>
+            </section>
+    
+            <section>
+                <h3>Dani</h3>
+                
+            </section>
+    
+             <section>
+                <h3>Benjamín</h3>
+
+            </section> 
+            
+            <section>
+                <h3>Rubén</h3>
+
+            </section>    
+    
+        </div>
+    </main>
